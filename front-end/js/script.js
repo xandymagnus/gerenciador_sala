604ca514269c70b52d7ec1e2b9b042bfee8a2818
xandymagnus <xandy.magno325@gmail.com>
2026-03-25 16:29:52 -0300

    filtro de aulas por horário e contador automático

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,4 +1,4 @@
-// Adicionando dia da semana interativo
+// Dias da semana interativo
 const dias = [
     "domingo",
     "SEGUNDA",
@@ -25,7 +25,8 @@ aulas.forEach(aula => {
 document.querySelectorAll("." + nomeDia).forEach(aula => {
     aula.style.display = "block";
 });
-// Criando lógica para trocar Manhã -> Noite assim que for 18:00
+
+// Trocar Manhã -> Noite assim que for 18:00
 const horaAtual = new Date().getHours();
 
 const titulo = document.querySelector(".titulo");
@@ -35,3 +36,36 @@ if (horaAtual >= 18) {
 } else {
     titulo.textContent = "QUADRO DE HORÁRIOS - MANHÃ";
 }
+
+// Exibe aula de acordo com horário
+
+const agora = new Date();
+
+const aulasHorario = document.querySelectorAll(".aula");
+
+aulasHorario.forEach(aula => {
+
+    const horario = aula.querySelector(".horario").textContent;
+
+    const partes = horario.split(" - ");
+    const horaFinal = partes[1];
+
+    const [hora, minuto] = horaFinal.split(":");
+
+    const fimAula = new Date();
+
+    fimAula.setHours(hora);
+    fimAula.setMinutes(minuto);
+    fimAula.setSeconds(0);
+
+    if (agora > fimAula) {
+        aula.parentElement.style.display = "none";
+    }
+
+});
+
+// Atualização contínua
+
+setInterval(() => {
+    location.reload();
+}, 60000);
