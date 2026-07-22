6c1b76022019587afbc00265219e6bb06ab01b61
Felipe Lopes <felopes__@hotmail.com>
2026-07-22 13:05:27 -0300

    Tela de adição criada e estilizada

--- front-end/js/admin.js
+++ front-end/js/admin.js
@@ -1,8 +1,24 @@
 // ================================
-// IMPORTAÇÕES
+// PUXANDO INFORMAÇÕES
 // ================================
+
+// BOTÕES 
+const botao = document.getElementById("addButton");
+const sair = document.getElementById("sair");
+
+// DIVS
+const overlay = document.getElementById("overlay");
+
+//INPUTS
 
 
 // ================================
-// CARREGAR JSON
+// CHAMANDO A TELA DE ADIÇÃO
 // ================================
+botao.addEventListener("click", () => {
+    overlay.classList.add("mostrar");
+});
+
+sair.addEventListener("click", () => {
+    overlay.classList.remove("mostrar");
+});
